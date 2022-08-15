bda4a766b6353de1ca70c4ec4c093ee440e045d8
Dennys Ferrer <dennys.ferrer@gmail.com>
2022-08-15 13:01:21 -0500

    Se trabajo el concepto de props y al mismo tiempo se introdujo el trabajo de css en react

---
+++ react-course/src/Button.js
@@ -0,0 +1,9 @@
+import PropTypes from "prop-types";
+
+export function Button({ text, name='User' }) {
+  return <button>{text} - {name}</button>;
+}
+
+Button.propTypes = {
+  text: PropTypes.string
+};
--- react-course/src/index.js
+++ react-course/src/index.js
@@ -1,17 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { Greeting, UserCard } from "./Greeting";
+import { Button } from "./Button";
+import { TaskCard } from "./Task";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
   <>
-    <Greeting title="Hola Mundo" />
+    {/* <Greeting title="Hola Mundo" />
     <Greeting title="Hola React" />
     <Greeting title="JavaScript" />
-    <Greeting title="Hola Dennys Ferrer" />
+    <Greeting title="Hola Dennys Ferrer" /> */}
 
-    <UserCard
+    {/* <UserCard
       name={"Dennys Ferrer"}
       amount={3000}
       married={true}
@@ -30,6 +32,15 @@ root.render(
       greet={function () {
         alert("Holaaa");
       }}
-    />
+    /> */}
+
+    {/* <Button text='Click-me'/>
+    <Button text='Pay'/>
+    <Button text={"Hola mundo"} name='Dennys Ferrer'/> */}
+
+    <TaskCard ready={false}/>
+
+
+
   </>
 );
